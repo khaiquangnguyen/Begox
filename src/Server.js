296b8e0d1ccfaa9a418f715d0b38a6f4f4ae7b66
khaiquangnguyen <kqn001@bucknell.edu>
2016-02-27 22:50:46 -0500

    update

--- src/Server.js
+++ src/Server.js
@@ -1,4 +1,5 @@
 //initiate the necessary requirements for the server
+var express = require('express');
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
@@ -9,15 +10,83 @@ http.listen(3000, function(){
 });
 
 //fetch index.html back to any client connect to the server through port 3000
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
-});
+
+app.use(express.static(__dirname + '/Client'));
+
+/*
+* Set up the necessary variables for the program to use
+*/
+//the array of socket
+var sockets = [];
+//the array of players
+var players = [];
+//the array of missiles
+var missiles = [];
+
+////////////////////////////////////////////////////////////
+
+/**
+ * The player object. Which is the tank
+ * @type {{id: number, xCor: number, yCor: number, width: number, height: number, canShoot: boolean}}
+ */
+var player = {
+    //the id of the player
+    id: 0,
+    //the x-center of the player
+    xCor: 0,
+    //the y-center of the player
+    yCor:0,
+    //the width of the player
+    width:0,
+    //the height of the player
+    height:0,
+    //check if the player can shoot or not
+    canShoot:true
+};
+
+/**
+ * The missile object, which is the projectile fired by players
+ * @type {{id: number, xCor: number, yCor: number, width: number, height: number}}
+ */
+var missile ={
+    //the id of the player which fires missile
+    id: 0,
+    //the x-center of the missile
+    xCor: 0,
+    //the y-center of the missile
+    yCor:0,
+    //the width of the missile
+    width:0,
+    //the height of the missile
+    height:0
+}
+
+
+/**
+ *  Handle everything which relates to sockets.io and socket.
+ * @param socket - the current connecting socket
+ */
+var connectionHandler = function(socket){
+
+
+    //FUNCTION DEFINITION
+
+    /**
+     * When the player changes direction
+     * @param newDirection - the new direction of the player
+     */
+    var changeDirection = function(newDirection){
+        socket.emit('changeDirection', newDirection);
+        console.log(newDirection);
+    };
+
+
+    //METHOD CALL
+
+    socket.on('changeDirection', changeDirection);
+}
 
 
 //socket.io job
-io.on('connection', function(socket){
-    socket.on('chat message', function(msg){
-        io.emit('chat message', msg);
-    });
-});
+io.on('connection', connectionHandler);
 
